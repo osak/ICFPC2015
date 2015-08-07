91d90047d69294971e78487d50f448fb8e3be152
Shunsuke Ohashi <shunsuke@indeed.com>
2015-08-07 11:03:35 -0500

    input_mapper.py: input validation

--- src/python/input_mapper.py
+++ src/python/input_mapper.py
@@ -5,12 +5,22 @@ sw =  frozenset({"a", "g", "h", "i", "j", "4"})
 se =  frozenset({"l", "m", "n", "o", " ", "5"})
 cw =  frozenset({"d", "q", "r", "v", "z", "1"})
 ccw = frozenset({"k", "s", "t", "u", "w", "x"})
+ignored = frozenset({"\r", "\n", "\t"})
 
-mapper = {w: 'w', e:'e', sw:'sw', se:'se', cw:'cw', ccw: 'ccw'}
+mapper = {w: 'w', e:'e', sw:'sw', se:'se', cw:'cw', ccw: 'ccw', ignored: 'ignored'}
+valid = frozenset()
+for cs in mapper:
+    valid = valid | cs
 
 if __name__ == '__main__':
     s = raw_input()
     for c in s:
+        if c not in valid:
+            print '!!! invalid character:', c
+            exit(1)
         for cset, name in mapper.items():
+            if name == 'ignored':
+                continue
             if c.lower() in cset:
                 print name
+
