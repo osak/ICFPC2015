13d6bdb7bf0b2005a1890aab248aeeb3b2f36615
Shunsuke Ohashi <shunsuke@indeed.com>
2015-08-07 18:09:06 -0500

    run.py: added sim runner which is available in any directory (still buggy).

---
+++ sim/run.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+import subprocess
+import os
+import sys
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+JAR_PATH = os.path.join(SCRIPT_DIR, 'sim.jar')
+LIB_PATH = os.path.join(SCRIPT_DIR, 'lib')
+args = ['java', '-cp', '"{0}:{1}/*"'.format(JAR_PATH, LIB_PATH), 'icfpc.Main'] + sys.argv[1:]
+print ' '.join(args)
+subprocess.call(args)
