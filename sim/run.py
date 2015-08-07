05b195bc027a4f3e4aa10a635f849ccca432a03f
Shunsuke Ohashi <shunsuke@indeed.com>
2015-08-07 18:11:34 -0500

    run.py: bug fixed

--- sim/run.py
+++ sim/run.py
@@ -6,6 +6,5 @@ import sys
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 JAR_PATH = os.path.join(SCRIPT_DIR, 'sim.jar')
 LIB_PATH = os.path.join(SCRIPT_DIR, 'lib')
-args = ['java', '-cp', '"{0}:{1}/*"'.format(JAR_PATH, LIB_PATH), 'icfpc.Main'] + sys.argv[1:]
-print ' '.join(args)
+args = ['java', '-cp', '{0}:{1}/*'.format(JAR_PATH, LIB_PATH), 'icfpc.Main'] + sys.argv[1:]
 subprocess.call(args)
