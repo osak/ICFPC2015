28e5a3b196a428a7fa8a439aa4d06539ee4d4831
Shunsuke Ohashi <shunsuke@indeed.com>
2015-08-08 00:25:46 -0500

    generate_cpp_input.py: create cpp input txt

---
+++ src/python/generate_cpp_input.py
@@ -0,0 +1,17 @@
+import translate
+from runner import get_path
+import json
+
+def main():
+    for i in xrange(24):
+        problem_path = get_path('../../problems', i)
+        with open(problem_path) as reader:
+            config = json.load(reader)
+        for seed in config['sourceSeeds']:
+            dest_path = '../../cpp_input/problem_{0}_{1}.txt'.format(i, seed)
+            with open(dest_path, 'w') as writer:
+                writer.write(translate.translate_single(config, seed))
+
+
+if __name__ == '__main__':
+    main()
