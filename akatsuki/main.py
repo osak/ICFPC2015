8778deb77d07d142fbb43e45764ce45a799cf80c
Shunsuke Ohashi <shunsuke@indeed.com>
2015-08-08 19:12:24 -0500

    Akatsuki: added output api

--- akatsuki/main.py
+++ akatsuki/main.py
@@ -44,6 +44,7 @@ def get_partial_game(prob_id, seed, rev, fr, to):
 def get_game(prob_id, seed, rev):
     return get_game_json(prob_id, seed, rev)
 
+
 @app.route("/board/<int:prob_id>/<int:seed>/<rev>/<int:turn>/")
 def get_board(prob_id, seed, rev, turn):
     client = pymongo.MongoClient(MONGO_URI)
@@ -56,6 +57,21 @@ def get_board(prob_id, seed, rev, turn):
         logger.warning('board not found.')
         return Response(json.dumps({'error': 'specified board not found.'}), content_type='application/json')
     logger.info('board found.')
+    return Response(json.dumps(document), content_type='application/json')
+
+
+@app.route("/output/<int:prob_id>/<int:seed>/<rev>/")
+def get_output(prob_id, seed, rev):
+    client = pymongo.MongoClient(MONGO_URI)
+    query = {'problemId': prob_id, 'seed': seed, 'revision': rev}
+    logger.info('/output api hit.')
+    logger.info('query: {}'.format(query))
+    document = client.kadingel.output.find_one(query, projection={'_id': False})
+    client.close()
+    if document is None:
+        logger.warning('output not found.')
+        return Response(json.dumps({'error': 'specified output not found.'}), content_type='application/json')
+    logger.info('output found.')
     return Response(json.dumps(document), content_type='application/json')
 
 
