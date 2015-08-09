3bdcfae4fefeba1d4aeccba9693434c74a2631d7
Shunsuke Ohashi <shunsuke@indeed.com>
2015-08-08 19:52:05 -0500

    Hibiki: initial commit/

---
+++ hibiki/main.py
@@ -0,0 +1,44 @@
+__author__ = 'shunsuke'
+
+from flask import Flask, render_template
+import pymongo
+from config import MONGO_URI, HIBIKI_HOST, HIBIKI_PORT
+import logging
+
+
+logger = logging.getLogger('hibiki')
+app = Flask(__name__)
+
+
+def get_all_output():
+    client = pymongo.MongoClient(MONGO_URI)
+    logger.info('collect all output.')
+    cursor = client.kadingel.output.find(projection={'_id': False})
+    documents = list(cursor.sort([('revision', pymongo.ASCENDING), ('problemId', pymongo.ASCENDING), ('seed', pymongo.ASCENDING)]))
+    client.close()
+
+    # filter duplicated docs
+    result = []
+    used_tuple = set()
+    for document in documents:
+        tup = (document['revision'], document['problemId'], document['seed'])
+        if tup not in used_tuple:
+            result.append(document)
+            used_tuple.add(tup)
+    return result
+
+
+@app.route("/")
+def index():
+    outputs = get_all_output()
+    base_url = 'http://icfpc.osak.jp/akatsuki/{api}/{problemId}/{seed}/{revision}/'
+    for output in outputs:
+        output['outputUrl'] = base_url.format(api='output', **output)
+        output['visdumpUrl'] = base_url.format(api='game', **output)
+    return render_template('index.html',
+                           outputs=outputs)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    app.run(host=HIBIKI_HOST, port=HIBIKI_PORT)
