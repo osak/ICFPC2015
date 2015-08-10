f0e697209430251935e756fbfe8588ddb16ddc56
Tomoyuki Hazehata <tomoyuki@indeed.com>
2015-08-10 03:42:35 -0500

    added spells

--- sim/src/icfpc/common/Spell.java
+++ sim/src/icfpc/common/Spell.java
@@ -7,6 +7,9 @@ public enum Spell {
     EI("ei!"),
     IAIA("ia! ia!"),
     RLYEH("r’lyeh"),
+    YUGGOTH("yuggoth"),
+    YOGSOTHOTH("yogsothoth"),
+    NECRONOMICON("necronomicon"),
     SENTENCE51("In his house at R'lyeh dead Cthulhu waits dreaming.");
 
     public final String phrase;
