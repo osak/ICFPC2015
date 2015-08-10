b9e25c910c564dc46799e6f7645e2f07fcc11cab
Osamu Koga <osak.63@gmail.com>
2015-08-10 00:42:21 -0500

    Fine: Ruby version must be 2.0 on ubuntu

--- fine/each.sh
+++ fine/each.sh
@@ -2,4 +2,4 @@
 
 NUM=`echo $1 | perl -pe 's/^.*_(\d+)\.json.*$/\1/'`
 echo $REVISION $COMMENT $BEGIN_DATE $NUM
-ruby fine/main.rb . "$REVISION" "$COMMENT" "$BEGIN_DATE" "$NUM" || true
+ruby2.0 fine/main.rb . "$REVISION" "$COMMENT" "$BEGIN_DATE" "$NUM" || true
