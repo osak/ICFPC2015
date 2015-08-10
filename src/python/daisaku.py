e644ed3f6aa9330914330cb19f9240bc2ccfd9ca
Shunsuke Ohashi <shunsuke@indeed.com>
2015-08-10 02:33:27 -0500

    Daisaku: fixed the bug when phrase contains ignore string

--- src/python/daisaku.py
+++ src/python/daisaku.py
@@ -6,19 +6,21 @@ from StringIO import StringIO
 
 def optimize(command, spell_list):
     moves = input_mapper.command_to_move(command)
-    spell_moves = [input_mapper.command_to_move(spell) for spell in spell_list]
+    spell_moves = [filter(lambda move: move != input_mapper.IGNORED, input_mapper.command_to_move(spell)) for spell in spell_list]
 
     optimized = StringIO()
     pos = 0
     while pos < len(moves):
         next_command = input_mapper.default_command(moves[pos])
+        progress = 1
         for i, spell in enumerate(spell_moves):
             npos = pos + len(spell)
             if moves[pos:npos] == spell:
                 next_command = spell_list[i]
+                progress = len(spell)
                 break
         optimized.write(next_command)
-        pos += len(next_command)
+        pos += progress
     return optimized.getvalue()
 
 
