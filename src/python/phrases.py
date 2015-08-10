94b968339bf9ffdb3574c3acf4bb308280d2f34a
Shunsuke Ohashi <shunsuke@indeed.com>
2015-08-10 02:54:59 -0500

    phrases.py: updated

--- src/python/phrases.py
+++ src/python/phrases.py
@@ -1,1 +1,1 @@
-known_phrases = ['ei!', 'ia! ia!', "r'lyeh", "in his house at r'lyeh dead cthulhu waits dreaming.", "yogsothoth"]
+known_phrases = ['ei!', 'ia! ia!', "r'lyeh", "in his house at r'lyeh dead cthulhu waits dreaming.", "yogsothoth", "yuggoth", "necronomicon"]
